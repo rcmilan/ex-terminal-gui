80dfe29687bc2512abd285019325c5c770dd5703
rcmilan <13418519+rcmilan@users.noreply.github.com>
2024-12-25 19:18:29 -0300

    Add project files.

---
+++ ex-terminal-gui/Program.cs
@@ -0,0 +1,10 @@
+﻿using ex_terminal_gui;
+using Terminal.Gui;
+
+Application.Init();
+
+Application.Run<ExampleWindow>();
+
+Application.Shutdown();
+
+Console.WriteLine($@"Username: {ExampleWindow.UserName}");
